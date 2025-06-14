cd7526d1033fd5d4c74a3cebdc00147170382f77
qauliyan <qauliyan627@gmail.com>
2025-06-14 14:28:54 +0800

    -c DATASET and var name

--- mode4_avgAll.py
+++ mode4_avgAll.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-DATASET = 2 # 選擇資料集
+DATASET = 1 # 選擇資料集
 COMP_MODE = 4
 ID = [186, 519, 563, 1, 165, 60, 544]
 LOCATION = f"SHAPSampling\\result_data\\{ID[DATASET]}"
-gapPath = f"{LOCATION}\\LOSS"
-with open(gapPath + f"\\loss_mode{COMP_MODE}.npy", 'rb') as file:
-    GAP_LIMIT = np.load(file, allow_pickle=True).item()
+lossPath = f"{LOCATION}\\LOSS"
+with open(lossPath + f"\\loss_mode{COMP_MODE}.npy", 'rb') as file:
+    LOSS_LIMIT = np.load(file, allow_pickle=True).item()
 
 mode4all = 0
 count = 0
-for i in GAP_LIMIT.values():
+for i in LOSS_LIMIT.values():
     count += 1
     mode4all += i
-print(GAP_LIMIT)
+print(LOSS_LIMIT)
 print(mode4all/count)
