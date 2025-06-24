4e1148547bc3e5af5adf085095a01e0dbd8f8d92
qauliyan <qauliyan627@gmail.com>
2025-06-24 14:28:39 +0800

    -c

--- plot.py
+++ plot.py
@@ -7,30 +7,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-ID = 563
-MODE = 0
+DATASET_NAME = "breast"
+SIM_TIME = 0
+MODE = 6
 COMP_MODE = 6
 ROUND = 50
-LOCATION = f"SHAPSampling\\result_data\\{ID}"
+SAMP = 120
+LOCATION = f"SHAPSampling\\result_data\\{DATASET_NAME}"
+if MODE == 6: ROUND = 1
+if MODE == 4: SAMP = 48
+
+def my_round(number, ndigits=0):
+    p = 10**ndigits
+    return (number * p * 2 + 1) // 2 / p
 
 def getAll_AllLossList_L2():# 計算AllLossList的L2
+    simTime = SIM_TIME
     i = 0
     avgL2 = 0
     loss = 0
     while True:
-        if os.path.exists(LOCATION + f"\\mode{MODE}\\AllLossList" + f"\\AllLossList_mode{MODE}_exd{i}_round{ROUND}.txt"):
+        allLossList_Path = LOCATION + f"\\simTime{simTime}\\mode{MODE}\\AllLossList\\AllLossList_mode{MODE}_exd{i}_round{ROUND}_samp{SAMP}.txt"
+        if os.path.exists(allLossList_Path):
             #輸入AllLossList
-            allLossList = np.loadtxt(LOCATION + f"\\mode{MODE}\\AllLossList" + f"\\AllLossList_mode{MODE}_exd{i}_round{ROUND}.txt")
+            allLossList = np.loadtxt(allLossList_Path, ndmin=1)
             for j in allLossList:
                 loss += j
             loss = loss/len(allLossList)
             avgL2 += loss**2
+            loss = 0
             i+=1
-        else: 
+        else:
             avgL2 = math.sqrt(avgL2)
-            print("ID:", ID, "MODE:", MODE)
-            print("avgL2 =", avgL2)
-            break
+            print("dataset:", DATASET_NAME, "MODE:", MODE, "simTime:", simTime, "avgL2 =", my_round(avgL2, 3))
+            i = 0
+            avgL2 = 0
+            loss = 0
+            simTime+=1
+        if not os.path.exists(LOCATION + f"\\simTime{simTime}"): break
 
 def get_AllLossList_L2(i):# 計算單筆AllLossList的L2
     loss = 0
@@ -48,7 +62,7 @@ def getLOSS(): # 計算lossLimit的L2
     for j in lossLimit.values():
         loss += j**2
     loss = math.sqrt(loss)
-    print("ID:", ID, "MODE:", MODE)
+    print("DATASET_NAME:", DATASET_NAME, "MODE:", MODE)
     print(sum(lossLimit.values())/len(lossLimit))
     print(loss)
 
